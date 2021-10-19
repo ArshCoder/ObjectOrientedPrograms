aae14f6e385e49acf597e3e574cefc9be8107733
CoderArsh <arshjeetb08@gmail.com>
2021-10-19 16:29:47 +0530

    Added: to check stock account

---
+++ StockInvetory.java
@@ -0,0 +1,41 @@
+package com.object_oriented_programs;
+
+import java.util.*;
+
+public class StockInvetory {
+
+	private static final int INVENTORY_SIZE = 12;
+	private Stock[] stocks;
+
+	public StockInvetory() {
+		stocks = new Stock[INVENTORY_SIZE];
+
+	}
+
+private static void StockInventory() {
+       for (int i = 0; i<INVENTORY_SIZE; i++){
+         Scanner console = new Scanner(System.in);
+
+    System.out.println ("Stock's name:");
+    String stockName = console.next();
+
+    System.out.println ("Stock's rating");
+    String stockRating= console.next();
+
+    System.out.println ("Stock's price:");
+    int stockPrice = console.nextInt();
+
+    System.out.println ("Numbers of shares: ");
+    int numberShares= console.nextInt();
+
+          Stocks [i]= new Stock(stockName, stockRatings, stockPrice, numberShares);
+    }
+
+public static void main (String [] args){
+	 StockInventory();
+
+
+
+	
+	}
+}
